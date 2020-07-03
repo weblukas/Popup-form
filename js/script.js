763faf1aaf596425d5a01d05209630aba5fbf909
Łukasz Szmyd <szmydlukasz30@gmail.com>
2020-07-03 16:42:18 +0200

    fetch method added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const popupOpenLink = document.getElementById('popup-link')
 const popupCloseBtn = document.querySelector('[data-close-button]')
 const overlay = document.getElementById('overlay')
 const popup = document.getElementById('popup')
+const form = document.querySelector('form')
+
+const nameInput = document.getElementById('name-input')
+
 
 popupOpenLink.addEventListener('click', () =>{
     openPopup(popup);
@@ -22,3 +26,25 @@ function closePopup(popup){
     popup.classList.remove('active');
     overlay.classList.remove('active');
 }
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const formData = new FormData(form);
+
+
+    // testowy url test.html
+    fetch('http://127.0.0.1:5500/test.html', {
+        method: 'post',
+        body: formData
+    }).then(function (response){
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+    }).catch(function (error) {
+        console.log(error);
+    })
+    
+
+})
+
